4404960339dc7362bffbbc0157defdb4a4b566ff
perezvonish <shokilak@gmail.com>
2026-01-16 01:45:49 +0500

    feat: resolved limit-service-time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,18 +10,62 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
+	sync.RWMutex
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
 }
 
+func (u *User) IncrementUsedTimeBySecond() {
+	u.Lock()
+	defer u.Unlock()
+
+	u.TimeUsed = u.TimeUsed + 1
+}
+
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	if !isLimitAvailable(u) {
+		return false
+	}
+
+	done := make(chan struct{}, 1)
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		process()
+		done <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return true
+		case <-ticker.C:
+			u.IncrementUsedTimeBySecond()
+			if !isLimitAvailable(u) {
+				return false
+			}
+		}
+	}
+}
+
+func isLimitAvailable(u *User) bool {
+	if !u.IsPremium && u.TimeUsed >= 10 {
+		return false
+	}
+
 	return true
 }
 
